4a6a7bad6c4d842a35c42293763a044967e36079
Francesc Martí Escofet <f.martiescofet@gmail.com>
2024-09-20 15:51:02 +0200

    Update script.py
    Co-authored-by: Kevin Klein <7267523+kklein@users.noreply.github.com>

--- script.py
+++ script.py
@@ -315,7 +315,7 @@ def predict_and_plot_cates(learner, covariates):
     fig, ax = plt.subplots(figsize=(10, 7))
     ax.scatter(cates, true_cate, alpha=0.5)
     ax.set_xlabel(
-        "$\\hat{\\tau}$: predicted difference in happiness \n (treatment effect)"
+        "$\\hat{\\tau}$: estimated difference in happiness \n (treatment effect)"
     )
     ax.set_ylabel("$\\tau$: difference in happiness \n (treatment effect)")
     fig.tight_layout()
